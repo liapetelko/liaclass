8bfebacb0995cbef03748e1456ce7eac3eefe79a
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-11 10:30:02 +0200

    Create a.py

---
+++ a.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Title")
+st.header("Header")
+st.success("Green")
+st.selectbox("select something", [1,2,3,4,5])
+st.checkbox("One")
+st.checkbox("Two")
+st.text_input("Enter something")
